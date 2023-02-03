110abb9e2979a2c76876fe3173ad0a4fe506ddcc
SebasMarquez <sebastian.marquez@correo.unimet.edu.ve>
2023-02-03 19:25:54 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const tittleElement = document.getElementById("MainTittle")
+const inputElement = document.getElementById("MainInput")
+const buttonElement = document.getElementById("SearchButton")
+
+buttonElement.addEventListener('click', () => {
+    const {value} = inputElement
+    //const value = inputElement.value
+})
